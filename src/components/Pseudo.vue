8ed32a87642c852c7719cfbe903c6314b1c79692
Bruno Follet-Locatelli <follet-bruno@hotmail.fr>
2021-10-28 16:38:55 +0200

    avec le fichier

---
+++ src/components/Pseudo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span>Pseudo : </span>
+    <input v-model="pseudo" />
+    <button v-on:click="updatePseudo">Valider</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pseudo: null,
+    };
+  },
+  methods: {
+    updatePseudo() {
+      this.$socket.client.emit("update_pseudo", this.pseudo);
+    },
+  },
+};
+</script>
+
+<style></style>
